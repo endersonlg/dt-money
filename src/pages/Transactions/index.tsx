a4c1062ffee173eabb78e40ab08932afe6e883b5
endersonlg <endersonluciog@gmail.com>
2023-04-07 21:45:33 -0300

    finish project

---
+++ src/pages/Transactions/index.tsx
@@ -0,0 +1,46 @@
+import { Header } from '../../components/Header'
+import { Summary } from '../../components/Summary'
+import { TransactionsContext } from '../../contexts/TransactionsContext'
+import { dateFormatter, priceFormatter } from '../../utils/formatter'
+import { SearchForm } from './components/SearchForm'
+import { useContextSelector } from 'use-context-selector'
+import {
+  PriceHighLight,
+  TransactionsTable,
+  TransactionsContainer,
+} from './styles'
+
+export function Transactions() {
+  const transactions = useContextSelector(
+    TransactionsContext,
+    (context) => context.transactions,
+  )
+
+  return (
+    <div>
+      <Header />
+      <Summary />
+
+      <TransactionsContainer>
+        <SearchForm />
+        <TransactionsTable>
+          <tbody>
+            {transactions.map((transaction) => (
+              <tr key={transaction.id}>
+                <td width="50%">{transaction.description}</td>
+                <td>
+                  <PriceHighLight variant={transaction.type}>
+                    {transaction.type === 'outcome' && '- '}
+                    {priceFormatter.format(transaction.price)}
+                  </PriceHighLight>
+                </td>
+                <td>{transaction.category}</td>
+                <td>{dateFormatter.format(new Date(transaction.createdAt))}</td>
+              </tr>
+            ))}
+          </tbody>
+        </TransactionsTable>
+      </TransactionsContainer>
+    </div>
+  )
+}
